b5abc1da94add52aa6dcb62595e03d8778e71f41
Volodymyr Smetana <vilsmet@gmail.com>
2022-12-25 15:26:19 +0100

    3

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,17 @@
 
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("modal__backdrop--is-hidden");
+    refs.modal.classList.toggle('modal__backdrop--is-hidden');
   }
 })();
-
 
 // (() => {
 //     const refs = {
